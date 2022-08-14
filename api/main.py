7a5a02fcae5e55378b40390c3b652fb674e0857d
Ragav Yarasi <ragav.code@gmail.com>
2022-08-14 14:13:47 +0530

    Add a placeholder API to receive attributes

--- api/main.py
+++ api/main.py
@@ -1,3 +1,5 @@
+# import pandas as pd
+# from pycaret.regression import load_model, predict_model
 from fastapi import FastAPI
 import uvicorn
 import os
@@ -11,6 +13,24 @@ host=str(os.getenv('HOST','0.0.0.0'))
 @app.get('/')
 def Home():
     return "Service is Running..."
+
+# This is meant to 
+@app.get('/options-attributes')
+def OptionsAttrubutes():
+    attributes = ['attribute_1', 'attribute_2', 'attribute_3', 'attribute_4', 'attribute_5']
+    return attributes
+
+# #. Load trained Pipeline
+# model = load_model('diamond-pipeline')
+
+# # Define predict function
+# @app.post('/predict')
+# def predict(carat_weight, cut, color, clarity, polish, symmetry, report):
+#     data = pd.DataFrame([[carat_weight, cut, color, clarity, polish, symmetry, report]])
+#     data.columns = ['Carat Weight', 'Cut', 'Color', 'Clarity', 'Polish', 'Symmetry', 'Report']
+
+#     predictions = predict_model(model, data=data) 
+#     return {'prediction': int(predictions['Label'][0])}
 
 
 if __name__ == "__main__":
