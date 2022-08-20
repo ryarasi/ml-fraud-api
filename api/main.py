ba8b45777ce333ecd1eece924194fcee370bd8d3
Ragav Yarasi <ragav.code@gmail.com>
2022-08-20 18:15:30 +0530

    Implemented postgres as specified here https://www.jeffastor.com/blog/pairing-a-postgresql-db-with-your-dockerized-fastapi-app

--- api/main.py
+++ api/main.py
@@ -2,8 +2,20 @@ from fastapi import FastAPI
 import uvicorn
 import os
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
+from core import config, tasks
+app = FastAPI(title=config.PROJECT_NAME, version=config.VERSION)
 
-app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.add_event_handler("startup", tasks.create_start_app_handler(app))
+app.add_event_handler("shutdown", tasks.create_stop_app_handler(app))
 
 port = int(os.getenv('PORT', 8000))
 reload = bool(os.getenv('RELOAD', 1))
